4f6a5e1488d3525fd292d3a990377d4b1cd8a7f3
0xK92JL4 <157831311+0xK92JL4@users.noreply.github.com>
2026-03-05 20:26:49 +0100

    added cmus listener

--- statusbar/status_bar.sh
+++ statusbar/status_bar.sh
@@ -11,6 +11,21 @@ update_status() {
     xsetroot -name "$song $updates $time_str"
 }
 
+# ===== CMUS listener in background =====
+(
+    last_song=""
+    while true; do
+        song=$(cmus-remote -Q 2>/dev/null | grep "^tag title " | cut -d' ' -f3-)
+
+        if [ "$song" != "$last_song" ]; then
+            last_song="$song"
+            reload_status_bar
+        fi
+
+        sleep 1
+    done
+) &
+
 trap 'update_status' SIGUSR1
 
 update_status
